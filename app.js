e2a4ed0701c29658f082da48f639961abe6cc0a2
Shivamthakur9777 <9777gmail.com>
2024-07-17 09:49:57 +0530

    Add file

---
+++ app.js
@@ -0,0 +1,18 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+function sendEmail() {
+    // Example: You can use AJAX to submit form data
+    // Replace this with your actual logic to send data
+    alert('Form submitted!'); // Replace with your actual form submission logic
+}
